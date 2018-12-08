d1207fe62217a33cc60581c4692b3319e6923c58
seen2 <tiwarietc@gmail.com>
2018-12-08 20:24:27 +0530

    completed almost speaker detail page

--- App.js
+++ App.js
@@ -3,34 +3,25 @@ import { StyleSheet, SectionList, View, Image, Text } from 'react-native';
 
 import contacts from './contacts';
 
-
-const sections=contacts.map(contact=>({title:contact.name,data:[contact.phone]}));
-const header=({section: {title}}) => (
-	<Text style={{fontWeight: 'bold',fontSize:20,}}>{title}</Text>
-)
-const makeContact=({item, index, section}) => (
-	<View style={styles.box}>
-		<Text key={index}>{item}</Text>
-		<Image 
-			source={require('./assets/Moab.jpeg')}
-			style={{flex:1,height:60,width:150}}
-		/>
-	</View>
-)
-
-const extractKey=(contact)=>String(contact.key); //expected key is of type string for FlatList
-
 export default class App extends React.Component {
 	render() {
     return (
 			<View style={styles.container}>
-				<SectionList
-					  renderItem={makeContact}
-						renderSectionHeader={header}
-						sections={sections}
-						keyExtractor={(item, index) => item + index}
-			/>
-
+				<View style={styles.detailsContainer}>
+					<Text style={styles.title}>Confrence Keynote </Text>
+					<Text style={styles.subtitle}>Thursday, May 18, 10-10:45 AM</Text>
+					<Text style={styles.description} >Hear about some stuff.</Text>
+				</View>
+				<View style={styles.speakerContainer}>
+					<Image 
+						style={styles.image}
+						source={require('./assets/Moab.jpeg')}
+					/>
+					<View style={styles.speakerDetailsContainer}> 
+						<Text style={styles.speakerName}> Lucy Vatne </Text>
+						<Text style={styles.speakerBio}> The best doggo </Text>
+					</View>
+				</View>
       </View>
     );
   }
@@ -40,23 +31,89 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
 		flex: 1,
-    backgroundColor: '#abce',
-    alignItems: 'center',
-		justifyContent: 'center',
+    backgroundColor: '#ffff',
 		paddingTop:25,
-  },
-	box: {
-		//flexGrow:0,
-		//flexShrink:1,
-		//flexBasis:100,//changes within ups and downs
-		flex: 1,
-		fontSize:15,
-		width:200,
-		borderWidth:3,
-		borderRadius:3,
+	},
+	detailsContainer:{
+		paddingVertical:30,
+		paddingHorizontal:20,
+		borderBottomWidth:StyleSheet.hairlineWidth,
 		borderColor:'grey',
-    backgroundColor:'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	},
+	title:{
+		fontSize:24,
+		fontWeight:'bold' || 500,
+		marginBottom:4,
+	},
+	subtitle:{
+		fontSize:15,
+		fontWeight:'bold' || 500,
+		marginBottom:12,
+	},
+	description:{
+		fontSize:13,
+	},
+	speakerContainer:{
+		paddingVertical:30,
+		paddingHorizontal:20,
+		flexDirection:'row',
+	},
+	image:{
+		height:80,
+		width:80,
+		borderRadius:40,
+		backgroundColor:'black',
+	},
+	speakerDetailsContainer:{
+		justifyContent:'center',
+	},
+	speakerName:{
+		fontSize:18,
+		color:'purple',
+	},
+	speakerBio:{
+		fontSize:13,
+	},
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//source={require('./assets/Moab.jpeg')}
+//
+//const sections=contacts.map(contact=>({title:contact.name,data:[contact.phone]}));
+/*
+const header=({section: {title}}) => (
+	<Text style={{fontWeight: 'bold',fontSize:20,}}>{title}</Text>
+)
+const makeContact=({item, index, section}) => (
+	<View style={styles.box}>
+		<Text key={index}>{item}</Text>
+		<Image 
+			style={{flex:1,height:60,width:150}}
+		/>
+	</View>
+)
+
+//const extractKey=(contact)=>String(contact.key); //expected key is of type string for FlatList
+*/
--- componentList/ScrollView/App.js
+++ componentList/ScrollView/App.js
@@ -1,51 +1,61 @@
 import React from 'react';
-import { StyleSheet, ScrollView, View, Image, Text } from 'react-native';
+import { StyleSheet, SectionList, View, Image, Text } from 'react-native';
 
 import contacts from './contacts';
 
+
+const sections=contacts.map(contact=>({title:contact.name,data:[contact.phone]}));
+const header=({section: {title}}) => (
+	<Text style={{fontWeight: 'bold',fontSize:20,}}>{title}</Text>
+)
+const makeContact=({item, index, section}) => (
+	<View style={styles.box}>
+		<Text key={index}>{item}</Text>
+		<Image 
+			source={require('./assets/Moab.jpeg')}
+			style={{flex:1,height:60,width:150}}
+		/>
+	</View>
+)
+
+const extractKey=(contact)=>String(contact.key); //expected key is of type string for FlatList
+
 export default class App extends React.Component {
 	render() {
-	
     return (
 			<View style={styles.container}>
+				<SectionList
+					  renderItem={makeContact}
+						renderSectionHeader={header}
+						sections={sections}
+						keyExtractor={(item, index) => item + index}
+			/>
 
-				<ScrollView>
-					{
-						contacts.map(contact=>MakeContact(contact))
-					}
-				</ScrollView>
       </View>
     );
   }
 }
 
-const MakeContact=(contact)=>(
-	<View key={contact.key}>
-		<Text> {contact.name} </Text>
-		<Image 
-			source={require('./assets/Moab.jpeg')}
-			style={{height:60,width:150}}
-		/>
-	</View>
-	)
 
 const styles = StyleSheet.create({
   container: {
 		flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#abce',
     alignItems: 'center',
-    justifyContent: 'center',
+		justifyContent: 'center',
+		paddingTop:25,
   },
 	box: {
 		//flexGrow:0,
 		//flexShrink:1,
 		//flexBasis:100,//changes within ups and downs
 		flex: 1,
+		fontSize:15,
 		width:200,
 		borderWidth:3,
 		borderRadius:3,
 		borderColor:'grey',
-    backgroundColor:'red',
+    backgroundColor:'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
